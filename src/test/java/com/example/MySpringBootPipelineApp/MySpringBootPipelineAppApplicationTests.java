73b26082281fb5e345eb32035f45852bdb0dc318
Toyin-Fadiran <toyin.fadiran@gmail.com>
2024-01-28 12:34:01 -0600

    updated pom to JDK17 and added test

--- src/test/java/com/example/MySpringBootPipelineApp/MySpringBootPipelineAppApplicationTests.java
+++ src/test/java/com/example/MySpringBootPipelineApp/MySpringBootPipelineAppApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.MySpringBootPipelineApp;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MySpringBootPipelineAppApplicationTests {
 
+	@Autowired
+	private NewsController newsController;
+	
 	@Test
 	void contextLoads() {
+		assertNotNull(newsController);
 	}
 
 }
